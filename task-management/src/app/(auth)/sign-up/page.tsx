53fdadbdf0dc84d3113b03fa80119facd44563d3
0XAymax <aymanderrouich3@gmail.com>
2025-03-26 12:11:30 +0000

    fixing logo switch between dark and light

--- task-management/src/app/(auth)/sign-up/page.tsx
+++ task-management/src/app/(auth)/sign-up/page.tsx
@@ -1,12 +1,18 @@
 import { SignUpForm } from "@/app/(auth)/sign-up/sign-up";
 import Logo from "@/components/Logo";
+import { useTheme } from "next-themes";
 
 export default function SignUp() {
+   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+   const { theme, resolvedTheme } = useTheme();
   return (
     <>
       <div className="flex items-center justify-center min-h-screen ">
         <div className="p-8 rounded-lg shadow-lg w-96 flex flex-col items-center">
-          <Logo theme={"dark"} isText={true} />
+          <Logo
+            theme={resolvedTheme == "light" ? "light" : "dark"}
+            isText={true}
+          />
           <SignUpForm />
         </div>
       </div>
--- task-management/src/app/page.tsx
+++ task-management/src/app/page.tsx
@@ -1,12 +1,16 @@
+"use client"
 import { LoginForm } from "@/components/login-form";
 import Logo from "@/components/Logo";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { theme, resolvedTheme } = useTheme();
   return (
     <>
       <div className="flex items-center justify-center min-h-screen ">
         <div className="p-8 rounded-lg shadow-lg w-96 flex flex-col items-center">
-          <Logo theme={"dark"} isText={true} />
+          <Logo theme={resolvedTheme == "light" ? "light" : "dark"} isText={true} />
           <LoginForm />
         </div>
       </div>
