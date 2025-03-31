f7f6ee21d6e5b76eaec0551ba2a5b1960e80e848
Marouane <marouanelemghari@gmail.com>
2025-03-31 20:50:45 +0000

    Added landing page

---
+++ task-management/src/app/(marketing)/page.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function LandingPage() {
+  return (
+    <div className="flex flex-col-reverse md:flex-row justify-center md:justify-between gap-6 min-h-[calc(100vh-64px)] px-2 md:px-10 md:pt-32">
+      <div className="text-center items-center justify-center md:text-start">
+        <h1 className="whitespace-nowrap">
+          Time to organize your{" "}
+          <span className="relative inline-block">
+            work{" "}
+            <Image
+              className="not-md:hidden absolute left-0 bottom-0"
+              width={120}
+              height={36}
+              src="/scribble.png"
+              alt="cross-out"
+            />{" "}
+            <span className="not-md:hidden absolute left-[50%] rotate-[-15deg] translate-y-[-80%] font-accent text-6xl text-[#7574C3]">
+              life
+            </span>
+          </span>
+        </h1>
+        <p>
+          Minimal task app which keeps track of all your projects in one place.
+        </p>
+        <div className="flex justify-center md:justify-start gap-5 pt-3">
+          <Button variant="outline">Take a Look</Button>
+          <Link href="/sign-up">
+            <Button>Get Started</Button>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Image
+          height={366}
+          width={650}
+          src="/illustration-woman-clipboard.png"
+          alt="Graphic illustration of a woman checking off items from a list from a clip-board"
+        />
+      </div>
+    </div>
+  );
+}
