35777f2065671a97c26d55f825fb1b89ad6c76e2
0XAymax <aymanderrouich3@gmail.com>
2025-03-31 10:11:00 +0000

    fixing switch mode position for the moment (to do later)

--- task-management/src/components/SwitchMode.tsx
+++ task-management/src/components/SwitchMode.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "@/components/ui/modeToggle";
 export default function SwitchMode() {
   return (
     <>
-      <div className="fixed mt-2.5 flex items-center gap-2">
+      <div className="fixed mt-2.5  ml-370 flex items-center gap-2">
         <ModeToggle />
       </div>
     </>
