ebf98c74619e31abea4ef89f06663b1a7c196e39
0XAymax <aymanderrouich3@gmail.com>
2025-04-01 14:44:58 +0000

    Adding Loading Skeleton for DashbordPage (it's not going to appear for the moment bcs we don't have an api call)

---
+++ task-management/src/app/dashboard/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SkeletonDashboard from './_dashbordComponents/Skeleton'
+
+function Loading() {
+  return (
+    <SkeletonDashboard/>
+  )
+}
+
+export default Loading
