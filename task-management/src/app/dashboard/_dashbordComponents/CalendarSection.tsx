b74c2dbfdb4f27fb5acc9253c83b851b104c8301
Marouane <marouanelemghari@gmail.com>
2025-04-02 21:58:51 +0000

    Renamed to CalendarSection and moved code to Calendar component

---
+++ task-management/src/app/dashboard/_dashbordComponents/CalendarSection.tsx
@@ -0,0 +1,13 @@
+import { Calendar } from "@/components/calendar";
+import React from "react";
+
+function CalendarSection() {
+  return (
+    <section>
+      <h2 className="mb-4 text-xl font-bold">Calendar</h2>
+      <Calendar />
+    </section>
+  );
+}
+
+export default CalendarSection;
