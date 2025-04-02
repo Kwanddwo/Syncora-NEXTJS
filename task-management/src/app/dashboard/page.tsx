19a113e8ffb7d40d7506ca9bf53e09f91af2c87d
Marouane <marouanelemghari@gmail.com>
2025-04-02 21:57:52 +0000

    changed breakpoint to md

--- task-management/src/app/dashboard/page.tsx
+++ task-management/src/app/dashboard/page.tsx
@@ -6,20 +6,19 @@ import Calendar from "./_dashbordComponents/Calendar";
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col gap-8 p-6">
-        {/* Recent Workspaces */}
-        <RecentWorkspaces />
+      {/* Recent Workspaces */}
+      <RecentWorkspaces />
 
-        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
-          {/* Your tasks for today */}
-          <TaskForToday />
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+        {/* Your tasks for today */}
+        <TaskForToday />
 
-          {/* Active Workspaces */}
-          <ActiveWorkspaces />
-        </div>
+        {/* Active Workspaces */}
+        <ActiveWorkspaces />
+      </div>
 
-        {/* Calendar */}
-        <Calendar />
- 
+      {/* Calendar */}
+      <Calendar />
     </div>
   );
 }
