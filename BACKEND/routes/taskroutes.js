e760d017eecd9ad4caf53009d934b213ce3f6038
FAHD4CH <chmanti.houari.fahd@gmail.com>
2025-04-03 15:12:46 +0100

    fixed taskhandleres and workspacehandlers issues

--- BACKEND/routes/taskroutes.js
+++ BACKEND/routes/taskroutes.js
@@ -1,10 +1,13 @@
-import { handleInputError } from "../middleware/middleware.js"
+import { handleInputError } from "../middleware/middleware.js";
 import { getAllTasks } from "../controllers/taskhandlers.js"
+import * as taskController from "../controllers/taskhandlers.js"
 import express from "express"
 const router = express.Router();
 
 // Middleware to verify token for all routes
 router.post('/tasks', getAllTasks); //ALL TASKS
+
+router.get('/usertasks', taskController.getTasksByUserId);
 
 /* router.get('/tasks/:id', taskController.getTaskById);
 router.post('/tasks', taskController.createTask);
