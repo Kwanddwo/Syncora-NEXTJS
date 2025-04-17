0c976c4e61015f1382f605ae1b7635d81bcb19d7
0XAymax <aymanderrouich3@gmail.com>
2025-04-17 16:03:24 +0100

    Adding Delete Workspace

--- task-management/src/app/dashboard/_dashbordComponents/ActiveWorkspaces.tsx
+++ task-management/src/app/dashboard/_dashbordComponents/ActiveWorkspaces.tsx
@@ -4,15 +4,19 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import { ChevronRight, ImageIcon } from "lucide-react";
+import {ChevronRight, ImageIcon, MoreVertical} from "lucide-react";
 import React, { useState } from "react";
 import {useWorkspaces} from "@/context/WorkspaceContext";
+import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import DeleteWorkspaceAlert from "@/app/dashboard/_dashbordComponents/_workspaceCrudComponents/workspaceDeleteAlert";
 
 interface OpenStates {
   [key: string]: boolean;
 }
 function ActiveWorkspaces() {
   const {workspaces}= useWorkspaces();
+  const userStr = localStorage.getItem("user");
+  const user = userStr ? JSON.parse(userStr) : null;
   const [openStates, setOpenStates] = useState(
     workspaces.reduce((acc: OpenStates, workspace) => {
       acc[workspace.id] = workspace.defaultOpen;
@@ -27,7 +31,7 @@ function ActiveWorkspaces() {
     }));
   };
 
-  return (
+    return (
     <section>
       <h2 className="mb-4 text-xl font-bold">Active Workspaces</h2>
       <div className="space-y-2">
@@ -51,6 +55,22 @@ function ActiveWorkspaces() {
                 <ImageIcon className="h-5 w-5" />
                 <span>{workspace.name}</span>
               </div>
+              {workspace.ownerId === user.id && (
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <div onClick={(e) => e.stopPropagation()} className="focus:outline-none">
+                        <MoreVertical className="h-4 w-4" />
+                      </div>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuItem
+                          className="text-red-500 focus:text-red-500" asChild
+                      >
+                        <DeleteWorkspaceAlert workspaceId={workspace.id} />
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+              )}
             </CollapsibleTrigger>
             <CollapsibleContent>
               {workspace.tasks?.length > 0 ? (
