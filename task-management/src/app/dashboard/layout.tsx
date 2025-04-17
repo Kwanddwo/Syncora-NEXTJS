2e49d284c561c631f077a3beb2ee939017ae85cb
0XAymax <aymanderrouich3@gmail.com>
2025-04-17 10:15:13 +0100

    Adding Workspace Context Provider

--- task-management/src/app/dashboard/layout.tsx
+++ task-management/src/app/dashboard/layout.tsx
@@ -4,18 +4,20 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import ReactQueryProvider from "@/components/providers/react-query-provider";
 import DashbordHeader from "./DashbordHeader";
 import ProtectedPage from "@/components/ProtectedPage";
-
+import { WorkspacesProvider } from "@/context/WorkspaceContext";
 function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <ProtectedPage>
         <ReactQueryProvider>
           <SidebarProvider>
+            <WorkspacesProvider>
             <AppSidebar />
             <SidebarInset>
               <DashbordHeader />
               {children}
             </SidebarInset>
+            </WorkspacesProvider>
           </SidebarProvider>
         </ReactQueryProvider>
       </ProtectedPage>
