a2d45c9f618bd9e2f6b2cbb7cc2045429ff8cc70
0XAymax <aymanderrouich3@gmail.com>
2025-04-21 10:41:58 +0100

    Adding RecentWorkspaces end point

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -5,6 +5,7 @@ import emailVrfRoute from "./routes/emailVrfRoute.js";
 import cookieParser from "cookie-parser";
 import taskroutes from "./routes/taskroutes.js"
 import workspaceRoutes from "./routes/workspaceRoutes.js"
+import recentWorkspaceRoutes from "./routes/recentWorkspaceRoutes.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -15,5 +16,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/emailverification",emailVrfRoute);
 app.use("/api/task",taskroutes);
 app.use("/api/workspace/",workspaceRoutes);
-
+app.use("/api/recentWorkspace", recentWorkspaceRoutes);
 app.listen(3001, () => console.log("Server running on port 3001"));
