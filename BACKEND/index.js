66f58a9e8848fd08ce0d68facc7c3bd7a994b5b7
0XAymax <aymanderrouich3@gmail.com>
2025-04-22 08:41:56 +0100

    removing unecessary shit

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,8 +1,5 @@
 import express from "express";
-import http from 'http';
-import { Server } from 'socket.io';
 import cors from "cors";
-import morgan  from "morgan"
 import authRoutes from "./routes/authRoutes.js";
 import emailVrfRoute from "./routes/emailVrfRoute.js";
 import cookieParser from "cookie-parser";
@@ -16,15 +13,14 @@ import recentWorkspaceRoutes from "./routes/recentWorkspaceRoutes.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true })) 
+app.use(express.urlencoded({ extended: true }))
 
-app.use("/api/auth", authRoutes);
-app.use("/api/emailverification",emailVrfRoute);
-app.use("/api/task",taskroutes);
-app.use("/api/workspace",workspaceRoutes);
-app.use("/api/invite",inviteRoutes);
+app.use("/api/auth", authRoutes)
+app.use("/api/emailverification", emailVrfRoute);
+app.use("/api/task", taskroutes);
+app.use("/api/workspace", workspaceRoutes);
+app.use("/api/invite", inviteRoutes);
 app.use("/api/recentWorkspace", recentWorkspaceRoutes);
 
 app.listen(3001, () => console.log("Server running on port 3001"));
