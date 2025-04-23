eb37857a3bb1823906a8aef2f6d0b7b35ea826fa
FAHD4CH <chmanti.houari.fahd@gmail.com>
2025-04-23 14:45:02 +0100

    Implement inbox functionality with routes and handlers; add invite details to inbox messages

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -9,6 +9,9 @@ import cookieParser from "cookie-parser";
 import taskroutes from "./routes/taskroutes.js"
 import workspaceRoutes from "./routes/workspaceRoutes.js"
 import inviteRoutes from "./routes/invitesroutes.js"
+import inboxRoutes from "./routes/inboxroutes.js"
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -21,6 +24,7 @@ app.use("/api/emailverification",emailVrfRoute);
 app.use("/api/task",taskroutes);
 app.use("/api/workspace",workspaceRoutes);
 app.use("/api/invite",inviteRoutes);
+app.use("/api/inbox",inboxRoutes);
 
 app.listen(3001, () => {
   console.log(`Server is running on port 3001`);
