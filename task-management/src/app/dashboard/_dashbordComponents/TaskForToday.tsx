81075a88b57e6eb378315e1830e26b9c3a8acc8e
0XAymax <aymanderrouich3@gmail.com>
2025-04-27 15:15:14 +0100

    Adding ScrollBar to TaskFortoday table

--- task-management/src/app/dashboard/_dashbordComponents/TaskForToday.tsx
+++ task-management/src/app/dashboard/_dashbordComponents/TaskForToday.tsx
@@ -9,12 +9,16 @@ import {
 } from "@/components/ui/table";
 
 import React from "react";
+import {ScrollArea} from "@/components/ui/scroll-area";
 
 function TaskForToday() {
   const tasks = [
     { id: "1", title: "Throw Trash", status: "To do", priority: "Medium" },
     { id: "2", title: "Pay Rent", status: "Done", priority: "Low" },
     { id: "3", title: "Make report", status: "On going", priority: "High" },
+    { id: "4", title: "Make Cake", status: "On going", priority: "High" },
+    { id: "5", title: "Chill", status: "On going", priority: "High" },
+    { id: "6", title: "Sleep", status: "On going", priority: "High" },
   ];
   const status = [
     { title: "To Do", style: "bg-gray-100" },
@@ -40,6 +44,7 @@ function TaskForToday() {
     <section>
       <h2 className="mb-4 text-xl font-bold">Your tasks for today</h2>
       <div className="rounded-md border">
+        <ScrollArea className="h-40">
         <Table>
           <TableHeader>
             <TableRow className="bg-gray-100">
@@ -48,27 +53,28 @@ function TaskForToday() {
               <TableHead>Priority</TableHead>
             </TableRow>
           </TableHeader>
-          <TableBody>
-            {tasks.map((task) => (
-              <TableRow key={task.id}>
-                <TableCell className="font-medium">{task.title}</TableCell>
-                <TableCell>
-                  <Badge
-                    variant="outline"
-                    className={getStatusStyle(task.status)}
-                  >
-                    {task.status}
-                  </Badge>
-                </TableCell>
-                <TableCell>
-                  <Badge className={getPriorityStyle(task.priority)}>
-                    {task.priority}
-                  </Badge>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
+            <TableBody>
+              {tasks.map((task) => (
+                  <TableRow key={task.id}>
+                    <TableCell className="font-medium">{task.title}</TableCell>
+                    <TableCell>
+                      <Badge
+                          variant="outline"
+                          className={getStatusStyle(task.status)}
+                      >
+                        {task.status}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>
+                      <Badge className={getPriorityStyle(task.priority)}>
+                        {task.priority}
+                      </Badge>
+                    </TableCell>
+                  </TableRow>
+              ))}
+            </TableBody>
         </Table>
+      </ScrollArea>
       </div>
     </section>
   );
