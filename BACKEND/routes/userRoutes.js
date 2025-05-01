84ff01442f7f6073f0bf4570021f4bc1fe085791
0XAymax <aymanderrouich3@gmail.com>
2025-05-01 12:02:10 +0100

    Applying fahd changes

--- BACKEND/routes/userRoutes.js
+++ BACKEND/routes/userRoutes.js
@@ -1,15 +1,22 @@
 import express from "express";
-
 import { handleInputError } from "../middleware/middleware.js";
 import * as userHandlers from "../controllers/userhandlers.js";
 import { authenticateUser } from "../middleware/middleware.js";
-export const routerr = express.Router();
+import { FindChecker } from "../middleware/usermiddleware.js";
+const router = express.Router();
 
-routerr.post(
+router.post("/UserDetails",
+  authenticateUser,
+  FindChecker,
+  userHandlers.getUserDetails);
+router.put("/UpdateDetails",
+  authenticateUser,
+  userHandlers.updateUserDetails);
+router.post(
   "/email",
   handleInputError,
   authenticateUser,
   userHandlers.getUsersByEmail
 );
 
-export default routerr;
+export default router;
