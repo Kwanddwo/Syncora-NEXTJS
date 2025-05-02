75b71e7d16b572de983ba83c57d683fc8972108f
0XAymax <aymanderrouich3@gmail.com>
2025-05-02 13:25:07 +0100

    Moving logo to sign-up component

--- task-management/src/app/(auth)/sign-up/page.tsx
+++ task-management/src/app/(auth)/sign-up/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 import { SignUpForm } from "@/app/(auth)/sign-up/sign-up";
-import Logo from "@/components/Logo";
 
 export default function SignUp() {
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen ">
-        <div className="p-8 rounded-lg shadow-lg w-96 flex flex-col items-center">
-          <Logo isText={true} hasBottomGutter={true} />
+      <div className="flex items-center flex-col justify-center min-h-screen p-24">
           <SignUpForm />
-        </div>
       </div>
     </>
   );
