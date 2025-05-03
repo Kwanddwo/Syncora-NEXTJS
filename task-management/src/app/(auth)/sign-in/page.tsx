db308b2b39327aa508d4ef06604c8b1eab32a734
0XAymax <aymanderrouich3@gmail.com>
2025-05-03 13:27:34 +0100

    Auto redirect to the dashboard if user is logged in

--- task-management/src/app/(auth)/sign-in/page.tsx
+++ task-management/src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,17 @@
 "use client";
 import { LoginForm } from "@/app/(auth)/sign-in/login-form";
 import Logo from "@/components/Logo";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
+import {useAuth} from "@/hooks/use-auth";
 export default function Home() {
+    const router = useRouter();
+    const { isAuthenticated } = useAuth();
+    useEffect(() => {
+        if(isAuthenticated){
+            router.push("/dashboard");
+        }
+    }, [isAuthenticated, router]);
   return (
     <>
       <div className="flex items-center justify-center min-h-screen ">
