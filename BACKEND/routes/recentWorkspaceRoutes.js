ee0477b57cd7c27ee176ade7e5d880281baa8f05
0XAymax <aymanderrouich3@gmail.com>
2025-05-03 23:34:27 +0100

    Adding membership verification in create recent workspace

--- BACKEND/routes/recentWorkspaceRoutes.js
+++ BACKEND/routes/recentWorkspaceRoutes.js
@@ -1,9 +1,10 @@
 import * as recentWorkspaceController from '../controllers/recentWorspacehandler.js';
 import { checkRecentWorkspaceExists } from '../middleware/RecentWorkspaceMiddleware.js'
+import * as workspaceMiddleware from "../middleware/workspacemiddleware.js";
 import express from 'express';
 const router = express.Router();
 
-router.post('/create',checkRecentWorkspaceExists, recentWorkspaceController.addRecentWorkspace);
+router.post('/create',checkRecentWorkspaceExists,workspaceMiddleware.verifyworkspace, recentWorkspaceController.addRecentWorkspace);
 router.post('/recent', recentWorkspaceController.getRecentWorkspaces);
 router.delete('/delete', recentWorkspaceController.removeRecentWorkspace);
 router.delete('/clear', recentWorkspaceController.clearRecentWorkspaces);
