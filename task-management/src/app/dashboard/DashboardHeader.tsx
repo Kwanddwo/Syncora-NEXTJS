357255f9eeb1be06480cece73219811b6e4132b7
0XAymax <aymanderrouich3@gmail.com>
2025-05-05 10:54:59 +0100

    Using profile context

--- task-management/src/app/dashboard/DashboardHeader.tsx
+++ task-management/src/app/dashboard/DashboardHeader.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -8,31 +9,79 @@ import { NotificationButton } from "@/components/notification-button";
 import { ModeToggle } from "@/components/ui/modeToggle";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { Building } from "lucide-react";
+import { LayoutDashboard, Mail } from "lucide-react";
 import React from "react";
+import { useProfile } from "@/context/ProfileContext";
+import AvatarUser from "@/components/Avatar-User";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger
+} from "@/components/ui/hover-card";
 
 function DashboardHeader() {
+  const { userDetailsState } = useProfile();
+
   return (
-    <header className="flex h-14 shrink-0 justify-between items-center gap-2 border-b px-3">
-      <div className="flex flex-1 items-center gap-2">
-        <SidebarTrigger />
-        <Separator orientation="vertical" className="mr-2 h-4" />
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbPage className="line-clamp-1 flex items-center gap-2">
-                <Building className="h-5 w-5" />
-                <span className="text-xl font-bold">Dashboard</span>
-              </BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-      </div>
-      <div className="flex items-center gap-2">
-        <NotificationButton />
-        <ModeToggle />
-      </div>
-    </header>
+      <header className="flex h-14 shrink-0 justify-between items-center gap-2 border-b px-3">
+        <div className="flex flex-1 items-center gap-2">
+          <SidebarTrigger />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbPage className="line-clamp-1 flex items-center gap-2">
+                  <LayoutDashboard className="h-5 w-5" />
+                  <span className="text-xl font-bold">Dashboard</span>
+                </BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+        <div className="flex items-center gap-2">
+          <HoverCard>
+            <HoverCardTrigger>
+              <div className="cursor-pointer">
+                <AvatarUser
+                    name={userDetailsState.name}
+                    lastName={userDetailsState.lastName}
+                    avatarUrl={userDetailsState.avatarUrl}
+                    height={8}
+                    width={8}
+                    borderSize={2}
+                    hasBorder={false}
+                />
+              </div>
+            </HoverCardTrigger>
+            <HoverCardContent className="w-80">
+              <div className="flex justify-between space-x-4">
+                <div className="flex items-center space-x-4">
+                  <AvatarUser
+                      name={userDetailsState.name}
+                      lastName={userDetailsState.lastName}
+                      avatarUrl={userDetailsState.avatarUrl}
+                      height={8}
+                      width={8}
+                      borderSize={2}
+                      hasBorder={false}
+                  />
+                  <div>
+                    <h4 className="text-sm font-semibold">{userDetailsState.name} {userDetailsState.lastName}</h4>
+                    {userDetailsState.email && (
+                        <div className="flex items-center text-sm text-muted-foreground">
+                          <Mail className="mr-1 h-3 w-3" />
+                          {userDetailsState.email}
+                        </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </HoverCardContent>
+          </HoverCard>
+          <NotificationButton />
+          <ModeToggle />
+        </div>
+      </header>
   );
 }
 
