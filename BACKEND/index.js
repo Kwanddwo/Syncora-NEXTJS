1e0b8afdf2ed83ea182706c4e5cddae519ddea47
Marouane <marouanelemghari@gmail.com>
2025-05-08 23:43:03 +0100

    added cors config

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -13,9 +13,20 @@ import inboxRoutes from "./routes/inboxroutes.js";
 import recentWorkspaceRoutes from "./routes/recentWorkspaceRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
+import dotenv from "dotenv";
+dotenv.config();
+// process.env.FRONTEND_URL
+console.log("Environment variables loaded:", process.env.FRONTEND_URL);
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, // single origin
+  // or for multiple:
+  // origin: ['https://site1.com','https://site2.com'],
+  optionsSuccessStatus: 200, // some legacy browsers choke on 204
+};
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
