97f35b6234959f6fa2091babdff9f689282645c8
Marouane Lemghari <110112853+Kwanddwo@users.noreply.github.com>
2025-06-24 15:43:53 +0100

    Deployment (#161)
    * Ignored typescript and eslint errors on build
    
    * Added heroku procfile
    
    * added cors config
    
    * fix: update CORS configuration and server port handling
    
    * Professional verification email
    
    ---------
    
    Co-authored-by: Marouane <marouanelemghari@gmail.com>

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -12,13 +12,30 @@ import inviteRoutes from "./routes/invitesroutes.js";
 import inboxRoutes from "./routes/inboxroutes.js";
 import recentWorkspaceRoutes from "./routes/recentWorkspaceRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-
 import { authenticateUser } from "./middleware/middleware.js";
 import feedbackHandler from "./controllers/feedbackHandler.js";
 
+import dotenv from "dotenv";
+dotenv.config();
+// process.env.FRONTEND_URL
+console.log("Environment variables loaded:", process.env.FRONTEND_URL);
+
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+// 1) Handle preflight for every path
+app.options("*", cors(corsOptions));
+
+// 2) Attach CORS headers to all routes
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
@@ -33,4 +50,6 @@ app.use("/api/recentWorkspace", recentWorkspaceRoutes);
 app.use("/api/user", userRoutes);
 app.post("/api/feedback", authenticateUser, feedbackHandler);
 
-app.listen(3001, () => console.log("Server running on port 3001"));
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
